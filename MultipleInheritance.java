91dcba8f67df35e61b400eb89a909a76b7e9bca6
Shubham Gupta <shubham3nemo@gmail.com>
2022-02-09 13:35:26 +0530

    Trying Multiple inheritance

---
+++ MultipleInheritance.java
@@ -0,0 +1,21 @@
+class Animal {
+	void method1() {
+		System.out.println("This is first parent class");
+	}
+}
+class FastAnimal {
+	void method2() {
+		System.out.println("This is second parent class ");
+	}
+}
+class tiger extends Animal , FastAnimal {
+	
+}
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
