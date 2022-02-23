4d8878952d5e4550857c73eb8bca0ceb1a93dfe1
Shubham Gupta <shubham3nemo@gmail.com>
2022-02-23 14:02:50 +0530

    Predicate Consumer

--- Predicate1.java
+++ Predicate1.java
@@ -1,6 +1,7 @@
 package Stream;
 import java.util.Collections;
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.function.*;
 public class Predicate1 {
 
@@ -9,8 +10,16 @@ public class Predicate1 {
 		int s = 5;
 		IntToDoubleFunction volCube = v -> (double)(s*s*s);
 		double result = volCube.applyAsDouble(s);
-		System.out.println();
+		System.out.println("The approxiamte volme "+result);
+		List<String> names = new ArrayList<String>();
+		Collections.addAll(names, "alok","Shubham","Ram","Sam");
+		Predicate<String> pd1 = (p) ->p.length()>10;
+		names.stream().filter(pd1).forEach(System.out::println);
+		
+		Consumer<String> con = cs -> System.out.println(cs+" Hello");
+		names.stream().forEach(con);
 
 	}
 
 }
+
