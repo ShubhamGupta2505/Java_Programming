299ffe9a19c03c8e4fed76fb2b0f3a7dccd23695
Shubham Gupta <shubham3nemo@gmail.com>
2022-02-28 13:27:35 +0530

    Reading and writing files

---
+++ ReadWriteFile.java
@@ -0,0 +1,16 @@
+package ExceptionHandling;
+import java.io.*;
+public class ReadWriteFile {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		fis = new FileInputStream("C:/oop/hi.txt");
+		int k;
+		
+		
+
+	}
+
+}
