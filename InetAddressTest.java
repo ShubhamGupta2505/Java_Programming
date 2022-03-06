0d28d6d609c398aeab3f6b248adc4d94828326e0
Shubham Gupta <shubham3nemo@gmail.com>
2022-03-06 16:54:23 +0530

    IP address of sites

---
+++ InetAddressTest.java
@@ -0,0 +1,15 @@
+package Stream;
+
+import java.net.*;
+class InetAddressTest
+{
+public static void main(String args[]) throws UnknownHostException {
+InetAddress Address = InetAddress.getLocalHost();
+System.out.println(Address);
+Address = InetAddress.getByName("www.HerbSchildt.com");
+System.out.println(Address);
+InetAddress SW[] = InetAddress.getAllByName("www.tecno.com");
+for (int i=0; i<SW.length; i++)
+System.out.println(SW[i]);
+}
+}
